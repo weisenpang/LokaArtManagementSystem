1c170d0002e8085dd6648c1471d31483e4ca7947
weisenpang <pangweisen@graduate.utm.my>
2025-08-23 23:37:18 +0800

    fix reset password
    rebuild reset password to be compatible with fetch function

--- src/controllers/userResetPasswordController.js
+++ src/controllers/userResetPasswordController.js
@@ -1,4 +1,4 @@
-import { userVerify } from "../models/User.js";
+import { User } from "../models/User.js";
 import { filePath } from '../config/filePath.js';
 //import express from "express";
 // const app = express()
@@ -7,20 +7,34 @@ import { filePath } from '../config/filePath.js';
 
 export const resetPassword = async (req, res) => {
     try {
-        const {token, email} = req.query;
         
-        const isVerified = await userVerify(email, token); // Call verifyUser to handle the request
-        console.log("isVerified from query:", isVerified);
-        if (!isVerified) {
-            
-            return res.status(400).json({ message: "Invalid or expired verification token." }); // Respond with an error if verification fails
-        }
-
-        console.log("isVerified:", isVerified); 
         res.sendFile(filePath('ResetPassword.html'));
         
     }catch (error) {
         console.error("Error during password reset:", error);
         res.status(500).json({ message: "Internal server error" });
     }
+}
+
+export const updatePassword = async (req, res) => {
+    const { password, email, token } = req.body;
+
+    const user = await User.findOne({ 
+        email,
+        verificationToken: token, 
+        verificationTokenExpires: { $gt: Date.now() } 
+
+     });
+    if (!user) {
+        return res.status(404).json({ message: "User not found." });
+    }else {
+        user.isVerified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpires = undefined;
+        user.password = password; // Update the user's password
+        await user.save(); // Save the updated user document
+        
+    }
+    
+    
 }
--- src/routes/signinRoutes.js
+++ src/routes/signinRoutes.js
@@ -2,9 +2,9 @@ import express from "express";
 import {filePath, filePathStatic} from '../config/filePath.js';
 import { verifyUser } from "../controllers/userSigninController.js";
 import { forgotPassword } from "../controllers/userForgotPasswordController.js";
-import { resetPassword } from "../controllers/userResetPasswordController.js";
+import { resetPassword, updatePassword } from "../controllers/userResetPasswordController.js";
 import path from 'path';
-import { User } from "../models/User.js";
+
 
 const signInRouter = express.Router();
 
@@ -14,22 +14,7 @@ signInRouter.get("/signin", (req,res) => {
 signInRouter.post("/signin",verifyUser);
 signInRouter.post("/forgot-password",forgotPassword);
 signInRouter.get("/reset-password",resetPassword);
-signInRouter.post("/reset-password", async (req, res) => {
-    const { password, confirmPassword } = req.body;
-
-    const email = req.body.email; // Assuming email is sent in the request body
-    const user = await User.findOne({ email });
-    if (!user) {
-        return res.status(404).json({ message: "User not found." });
-    }
-    if (password !== confirmPassword) {
-        return res.status(400).json({ message: "Passwords do not match." });
-    }else {
-        user.password = password; // Update the user's password
-        await user.save(); // Save the updated user document
-        return res.redirect('/'); // Redirect to the sign-in page after successful password reset
-    }
-});
+signInRouter.post("/reset-password", updatePassword);
 
 console.log(path.join(filePathStatic, 'css/main.css')); // Verify the path
 
